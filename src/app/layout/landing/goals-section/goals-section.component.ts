26ffb35627fba5b933ea70def3de24ec3a90c53e
Godwin-munan <godwinjethro@gmail.com>
2025-04-08 08:47:58 +0100

    Created sections skeletal components

---
+++ src/app/layout/landing/goals-section/goals-section.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-goals-section',
+  standalone: false,
+  templateUrl: './goals-section.component.html',
+  styleUrl: './goals-section.component.scss'
+})
+export class GoalsSectionComponent {
+
+}
---
+++ src/app/layout/landing/vision-section/vision-section.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-vision-section',
+  standalone: false,
+  templateUrl: './vision-section.component.html',
+  styleUrl: './vision-section.component.scss'
+})
+export class VisionSectionComponent {
+
+}
