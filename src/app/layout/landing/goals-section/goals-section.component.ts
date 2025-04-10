49f3d7fa940a465d60b6bdacf16b93aa46529404
Godwin-munan <godwinjethro@gmail.com>
2025-04-10 15:22:42 +0100

    Goals section impl v2

--- src/app/layout/landing/goals-section/goals-section.component.ts
+++ src/app/layout/landing/goals-section/goals-section.component.ts
@@ -8,4 +8,78 @@ import { Component } from '@angular/core';
 })
 export class GoalsSectionComponent {
 
+  isContentVisible = false;
+  isLeftContentVisible = false;
+  isRightContentVisible = false;
+  isClicked = false;
+  isLeftClicked = false;
+  isRightClicked = false;
+
+
+  onMouseEnter() {
+    if (!this.isClicked) {
+      this.isContentVisible = true;
+    }
+
+    if(!this.isRightClicked){
+      this.isRightContentVisible = true;
+    }
+
+    if(!this.isLeftClicked){
+      this.isLeftContentVisible = true;
+    }
+  }
+
+  onLeftMouseEnter() {
+
+    if(!this.isLeftClicked){
+      this.isLeftContentVisible = true;
+    }
+  }
+
+  onRightMouseEnter() {
+    
+    if(!this.isRightClicked){
+      this.isRightContentVisible = true;
+    }
+
+  }
+
+  onLeftMouseLeave() {
+
+    if(!this.isLeftClicked){
+      this.isLeftContentVisible = false;
+    }
+  }
+
+  onRightMouseLeave() {
+
+    if(!this.isRightClicked){
+      this.isRightContentVisible = false;
+    }
+
+  }
+
+  onTouchStart(event: TouchEvent) {
+    event.stopPropagation(); // Prevents the event from bubbling up
+    this.isClicked = !this.isClicked;
+    this.isContentVisible = this.isClicked;
+
+
+  }
+
+  onLeftTouchStart(event: TouchEvent){
+    event.stopPropagation();
+
+    this.isLeftClicked = !this.isLeftClicked;
+    this.isLeftContentVisible = this.isLeftClicked;
+  }
+
+  onRightTouchStart(event: TouchEvent){
+    event.stopPropagation();
+    
+    this.isRightClicked = !this.isRightClicked;
+    this.isRightContentVisible = this.isRightClicked;
+  }
+
 }
