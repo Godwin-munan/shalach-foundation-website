7950fffbfd1101c322d88d52b5b76012c682af17
Godwin-munan <godwinjethro@gmail.com>
2025-04-17 23:32:34 +0100

    Add routing to new pages

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,6 +1,8 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { LandingComponent } from './layout/landing/landing/landing.component';
+import { TeamComponent } from './pages/team/team.component';
+import { OverviewComponent } from './pages/overview/overview.component';
 
 const routes: Routes = [
   {
@@ -9,13 +11,26 @@ const routes: Routes = [
     pathMatch: 'full'
   },
   {
+    path: 'team',
+    component: TeamComponent
+  },
+  {
+    path: 'overview',
+    component: OverviewComponent
+  },
+  {
     path: '**',
     redirectTo: ''
   }
 ];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: [RouterModule.forRoot(routes, 
+    {
+      anchorScrolling: 'enabled',
+      onSameUrlNavigation: 'reload'
+    }
+  )],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
