e3a3b21c5b736076ee705ffd2d1a2d8a2117ae41
Godwin-munan <godwinjethro@gmail.com>
2025-04-18 15:54:14 +0100

    Footer section update - fixed max-length issue for mobile view

--- src/app/layout/landing/footer-section/footer-section.component.ts
+++ src/app/layout/landing/footer-section/footer-section.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { NavigationEnd, Router } from '@angular/router';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-footer-section',
@@ -9,4 +11,30 @@ import { Component } from '@angular/core';
 export class FooterSectionComponent {
 
   currentYear: number = new Date().getFullYear();
+  isHomePage: boolean = false;
+
+    private routerSubscription: Subscription | undefined;
+  
+    constructor(private router: Router) {}
+
+    ngOnInit() {
+    
+      // Subscribe to router events to update isHomePage on every navigation
+      this.routerSubscription = this.router.events.subscribe(event => {
+        if (event instanceof NavigationEnd) {
+          this.isHomePage = event.urlAfterRedirects === '/';
+    
+        }
+      });
+    
+      // Also check the current route immediately on init
+      this.isHomePage = this.router.url === '/';
+    }
+  
+    ngOnDestroy() {
+      if (this.routerSubscription) {
+        this.routerSubscription.unsubscribe();
+      }
+    }
+
 }
